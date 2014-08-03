f7e36ffd8354021f1a54f1b60fed99716c382af2
Eric Schles <ericschles@gmail.com>
2014-08-03 09:37:18 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Eric";
+            string lastName = "Schles";
+
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+
+            Console.WriteLine("Please enter a new first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Please enter a new last name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine("New name: " + firstName + " " + lastName);
+            Console.ReadLine();
+        }
+    }
+}
